2cc72a049e48239dd90ae84005cc0236155f6822
Marcos Lopes <sl.marcos15@hotmail.com>
2021-03-18 21:50:23 -0300

    ✅ ensure RestLoadStarships throws if loadStarshipRequest throws

--- api/tests/data/use-cases/rest-load-starship.spec.ts
+++ api/tests/data/use-cases/rest-load-starship.spec.ts
@@ -1,5 +1,6 @@
 import { RestLoadStarships } from '@/data/use-cases'
 import { LoadStarshipRequestSpy } from '@/tests/data/mocks'
+import { throwError } from '@/tests/domain/helpers'
 
 type SutTypes = {
   sut: RestLoadStarships
@@ -20,5 +21,12 @@ describe('RestLoadStarships use case', () => {
     const { sut, loadStarshipRequestSpy } = makeSut()
     await sut.load()
     expect(loadStarshipRequestSpy.calls).toBe(1)
+  })
+
+  test('Should throws if loadStarshipRequest throws', async () => {
+    const { sut, loadStarshipRequestSpy } = makeSut()
+    jest.spyOn(loadStarshipRequestSpy, 'load').mockImplementationOnce(throwError)
+    const promise = sut.load()
+    await expect(promise).rejects.toThrow()
   })
 })
