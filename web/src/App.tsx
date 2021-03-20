6036af83166d50b4d608cc3c7f902b70e26584bf
Marcos Lopes <sl.marcos15@hotmail.com>
2021-03-20 19:37:43 -0300

    ✨ add home page route

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import HomePage from './pages/home'
 import './App.css'
 
 function App (): JSX.Element {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route path='/' component={HomePage} exact />
+        </Switch>
+      </Router>
     </div>
   )
 }
